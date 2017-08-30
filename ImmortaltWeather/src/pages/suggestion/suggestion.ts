351079bd70d24de45718cebc2ac8aa2fdb82580d
Immortalt <tianqihang@126.com>
2017-08-30 22:21:38 +0800

    Revert "Revert "完善外出推荐""
    This reverts commit 9eff27d3db854e385a7694f5fe765cb78be562c9.

--- ImmortaltWeather/src/pages/suggestion/suggestion.ts
+++ ImmortaltWeather/src/pages/suggestion/suggestion.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { WeatherService } from '../../providers/providers';
+import { AppConfig } from '../../app/app.config';
+import { HeWeather5 } from '../../models/HeWeather5';
 @Component({
   selector: 'page-suggestion',
   templateUrl: 'suggestion.html'
 })
 export class SuggestionPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) { }
-
+  cityname: string;
+  suggestion:HeWeather5.Suggestion = {} as HeWeather5.Suggestion;//生活建议
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public weatherService: WeatherService) { }
+    ionViewDidLoad() {
+      this.uopdateWeather(false);
+    }
+    //更新天气
+    //force：是否强制刷新
+    async uopdateWeather(force: boolean) {
+      if (AppConfig.weatherData == null || force) {//如果需要重新获取数据
+        AppConfig.cityname = await this.weatherService.getCity();
+        console.log('AppConfig.cityname', AppConfig.cityname);
+        if (AppConfig.cityname != null) {
+          this.cityname = AppConfig.cityname;
+          AppConfig.weatherData = await this.weatherService.getWeatherData(AppConfig.cityname) as HeWeather5.Data;
+          console.log('AppConfig.weatherData', AppConfig.weatherData);
+          if (AppConfig.weatherData != null) {
+            this.loadWeather();
+          } else {
+            alert('获取天气失败！');
+          }
+        } else {
+          alert('定位城市失败！');
+        }
+      } else {
+        this.loadWeather();
+      }
+    }
+    //载入天气
+    loadWeather() {
+      this.suggestion = AppConfig.weatherData.suggestion;
+    }
 }

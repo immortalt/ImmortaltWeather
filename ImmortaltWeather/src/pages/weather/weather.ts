be17d4dd071227b6443f387d4f2a8bdb7193f207
LinFengYnu <ynublow@gmail.com>
2017-09-04 09:15:57 +0800

    修复图片加载不出来的bug

--- ImmortaltWeather/src/pages/weather/weather.ts
+++ ImmortaltWeather/src/pages/weather/weather.ts
@@ -18,33 +18,6 @@ export class WeatherPage {
   city: HeWeather5.City = {} as HeWeather5.City;//城市空气指数
   wind: HeWeather5.Wind3 = {} as HeWeather5.Wind3;//风向
 
-
-  estateProperty = {
-    name: 'Pretty house',
-    description: `It’s a 2 bedroom, 2 bathroom laneway house that also has a spacious study off the upstairs landing.
-                  Sporting modern finishes and some cute touches like wall niches and bamboo accents,
-                  this laneway house is a great example of what can be built on most of Vancouver’s standard 33 x 122
-                  foot lots.`,
-    price: '850000',
-    image: 'https://www.smallworks.ca/wp-content/uploads/exterior11.jpg',
-    style: 'Modern Interior',
-    size: '33\' Lot',
-    features: [
-      {
-        icon: 'paw',
-        title: 'Pet Friendly'
-      },
-      {
-        icon: 'bicycle',
-        title: 'Bike Stations'
-      },
-      {
-        icon: 'rose',
-        title: 'Beautiful Garden'
-      }
-    ]
-  }
-
   constructor(public navCtrl: NavController, public modalCtrl: ModalController
     , public weatherService: WeatherService) {
   }
